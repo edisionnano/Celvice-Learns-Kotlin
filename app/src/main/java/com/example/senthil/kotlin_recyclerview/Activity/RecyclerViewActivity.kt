822ba062530a6109fefed9867f107f4b145d4db6
spiros <pineaea@gmail.com>
2021-04-13 22:48:40 +0300

    war for territorry

--- app/src/main/java/com/example/senthil/kotlin_recyclerview/Activity/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/senthil/kotlin_recyclerview/Activity/RecyclerViewActivity.kt
@@ -6,12 +6,14 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.LinearLayout
-import com.example.senthil.kotlin_recyclerview.Adapter.CustomRecyclerAdapter
+import android.widget.Toast
 import com.example.senthil.kotlin_recyclerview.Model.AndroidVersionModel
 import com.example.senthil.kotlin_recyclerview.R
 import com.example.senthil.kotlin_recyclerview.Utils.Helper
 import org.jsoup.Jsoup
 import java.io.IOException
+import com.example.senthil.kotlin_recyclerview.Adapter.CustomRecyclerAdapter
+import kotlin.concurrent.fixedRateTimer
 
 class RecyclerViewActivity : AppCompatActivity() {
 
@@ -20,23 +22,39 @@ class RecyclerViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_recycler_view)
         val rvRecyclerView = findViewById<RecyclerView>(R.id.sample_recyclerView)
         rvRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
-        WebScratch().execute()
-        var adapter = CustomRecyclerAdapter(this, Helper.getVersionsList())
+        var adapter = CustomRecyclerAdapter(this, Helper.Companion.getVersionsList())
         rvRecyclerView.adapter = adapter
+        fixedRateTimer("timer",false,0,10000){
+            this@RecyclerViewActivity.runOnUiThread {
+                //Toast.makeText(this@RecyclerViewActivity, "text", Toast.LENGTH_SHORT).show()
+                adapter.notifyDataSetChanged()
+            }
+        }
+        fixedRateTimer("timer",false,0,60000){
+            this@RecyclerViewActivity.runOnUiThread {
+                //Toast.makeText(this@RecyclerViewActivity, "text", Toast.LENGTH_SHORT).show()
+                WebScratch().execute()
+            }
+        }
     }
 
     inner class WebScratch : AsyncTask<Void, Void, Void>() {
         private lateinit var words: String
         override fun doInBackground(vararg params: Void): Void? {
             try {
+                Helper.matchList.clear()
                 val document =  Jsoup.connect("https://greeklivechannels.ml/livematches.html").get().run {
                     select("div.match").forEachIndexed { index, element ->
-                        val titleAnchor = element.select("h3 a")
-                        val title = titleAnchor.text()
-                        val url = titleAnchor.attr("href")
-                        //3. Dumping Search Index, Title and URL on the stdout.
-                        Helper.matchList.add(AndroidVersionModel(R.drawable.superleague, "Ολυμπιακός - Παναθηναίκός", "Super League", soundLang = "Ελληνικό", matchTime = "10:00"))
-                        println("$index. $title ($url)")
+                        val championship = element.select("h3").get(0).text()
+                        val sound = element.select("h3").get(1).text()
+                        val teams = element.select("h3").get(2).text()
+                        val hour = element.select("h3").get(3).text()
+                        val links = element.select("a").size
+                        for (i in 1..(element.select("a").size) step 1) {
+                            var link = element.select("a").get(i-1).attr("href")
+                            println("$link")
+                        }
+                        Helper.matchList.add(AndroidVersionModel(R.drawable.championsleague, "$teams", "$championship", "$sound", "$hour"))
                     }
                 }
             } catch (e: IOException) {
